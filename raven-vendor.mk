c2323454f07d5c8a8160d06cedfb6fcfe85d55e3
Pimpmaneaton <shaunsibu@gmail.com>
2024-09-22 05:37:51 -0600

    Update raven-vendor.mk, remove HBM

--- raven-vendor.mk
+++ raven-vendor.mk
@@ -69,7 +69,6 @@ PRODUCT_PACKAGES += \
     libmediaadaptor \
     com.google.edgetpu_app_service-V3-ndk \
     com.google.edgetpu_vendor_service-V2-ndk \
-    libhbmsvmanager_jni \
     VZWAPNLib \
     AppDirectedSMSService \
     CarrierSettings_signed_45.0.591154042 \
@@ -94,7 +93,6 @@ PRODUCT_PACKAGES += \
     EuiccGoogleOverlay \
     EuiccSupportPixel \
     EuiccSupportPixelPermissions \
-    HbmSVManager \
     MyVerizonServices \
     OemRilService \
     RilConfigService \
